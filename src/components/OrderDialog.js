19e6b23025215a6d1792e67d4433d7b4f70bca90
cordobadigitalmedia <ismael@cordobadigitalmedia.com>
2021-04-10 08:57:19 +0300

    added order form

--- src/components/OrderDialog.js
+++ src/components/OrderDialog.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { add, remove } from "../store/cart.store";
 import { withStyles } from "@material-ui/core/styles";
+import { DateTime } from "luxon";
 import {
   CircularProgress,
   Button,
@@ -10,12 +11,25 @@ import {
   DialogContent,
   TextField,
   Box,
+  CardHeader,
+  CardContent,
+  Card,
   Typography,
   IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  ListItemSecondaryAction,
+  Divider,
 } from "@material-ui/core";
 import MuiDialogTitle from "@material-ui/core/DialogTitle";
 import Alert from "@material-ui/lab/Alert";
 import CloseIcon from "@material-ui/icons/Close";
+import PersonAddIcon from "@material-ui/icons/PersonAdd";
+import PinDropIcon from "@material-ui/icons/PinDrop";
+import { MonetizationOn } from "@material-ui/icons";
 import _ from "lodash";
 
 const styles = (theme) => ({});
@@ -49,6 +63,23 @@ function OrderDialog(props) {
     setshowDialog(false);
   };
 
+  const findPickupDate = () => {
+    const dayINeed = 6; // for Thursday
+    const today = DateTime.now().weekday;
+
+    if (today < dayINeed) {
+      return DateTime.now()
+        .plus({ days: dayINeed - today })
+        .toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY);
+    } else if (today === dayINeed) {
+      return DateTime.now().toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY);
+    } else {
+      return DateTime.now()
+        .plus({ days: 7 - (today - dayINeed) })
+        .toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY);
+    }
+  };
+
   return (
     <Box>
       <Box display="flex" flexDirection="row">
@@ -74,11 +105,121 @@ function OrderDialog(props) {
           Place Order
         </DialogTitle>
         <DialogContent dividers={true}>
-            <p>List of items with total price</p>
-            <p>Email/Name/Phone Fields</p>
-            <p>Submit</p>
-            <p>Alert - pickup date/time range location</p>
+          <Box pb={1}>
+            <Card>
+              <CardHeader
+                avatar={
+                  <Avatar aria-label="person">
+                    <PersonAddIcon />
+                  </Avatar>
+                }
+                title={
+                  <Typography variant="h6" component="h4">
+                    Personal Information
+                  </Typography>
+                }
+              />
+              <CardContent>
+                <Box>
+                  <Box pb={1}>
+                    <TextField id="name" label="Name" variant="outlined" />
+                  </Box>
+                  <Box>
+                    <TextField id="email" label="E-mail" variant="outlined" />
+                  </Box>
+                </Box>
+              </CardContent>
+            </Card>
+          </Box>
+          <Box pb={1}>
+            <Card>
+              <CardHeader
+                avatar={
+                  <Avatar aria-label="address">
+                    <PinDropIcon />
+                  </Avatar>
+                }
+                title={
+                  <Typography variant="h6" component="h4">
+                    {`Pickup on ${findPickupDate()}`}
+                  </Typography>
+                }
+              />
+              <CardContent>
+                <Typography variant="body1" component="p">
+                  Please pickup and pay for your items below at Inaya
+                  Permaculture's home organic farmers market on{" "}
+                  <b>{findPickupDate()}</b> from 11:00 AM to 7:00 PM
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+          <Box pb={1}>
+            <Card>
+              <CardHeader
+                avatar={
+                  <Avatar aria-label="address">
+                    <MonetizationOn />
+                  </Avatar>
+                }
+                title={
+                  <Typography variant="h5" component="h3">
+                    Order Summary
+                  </Typography>
+                }
+              />
+              <CardContent>
+                <List>
+                  {props.cartItems.items.map((item, i) => (
+                    <div key={item}>
+                      <ListItem>
+                        <ListItemAvatar>
+                          <Avatar>{`${item.count}x`}</Avatar>
+                        </ListItemAvatar>
+
+                        <ListItemText
+                          primary={
+                            <React.Fragment>
+                              <div>{item.product.fields.Name}</div>
+                            </React.Fragment>
+                          }
+                          secondary={
+                            <React.Fragment>
+                              <div>
+                                {`Amount: ${
+                                  item.product.fields["Price / Kg"] * item.count
+                                } JOD`}
+                              </div>
+                            </React.Fragment>
+                          }
+                        />
+                      </ListItem>
+                      <Divider />
+                    </div>
+                  ))}
+                  <ListItem>
+                    <ListItemSecondaryAction>
+                      <Box pt={2}>
+                        <Typography variant="h6" component="h4">
+                          {`Total: ${props.cartItems.totalAmount} JOD`}
+                        </Typography>
+                      </Box>
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                </List>
+              </CardContent>
+            </Card>
+          </Box>
         </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => handleClick()}
+          >
+            Submit Order Now
+          </Button>
+        </DialogActions>
       </Dialog>
     </Box>
   );
